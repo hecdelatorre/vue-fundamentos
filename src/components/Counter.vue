dfc5f0a76b7a0d15431145a9edc4523341614746
hecdelatorre <hector982015@gmail.com>
2023-01-16 23:53:08 -0600

    16-01-2023 23:53:08 Computed Properties

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,11 +1,25 @@
 <template>
   <h2>Counter</h2>
-  <p>98</p>
+  <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      counter: 5
+    }
+  },
+  methods: {
+    getSquareValue() {
+      return Math.pow(this.counter, 2)
+    }
+  },
+  computed: {
+    squareCounter() {
+      return Math.pow(this.counter, 2)
+    }    
+  }
 }
 </script>
 
