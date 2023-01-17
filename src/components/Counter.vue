c3ebd1b60a84a23e2897561a338267db964c0ca1
hecdelatorre <hector982015@gmail.com>
2023-01-17 12:24:21 -0600

    17-01-2023 12:25:57 increase decrease

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,6 +1,12 @@
 <template>
-  <h2>Counter</h2>
+  <h2>Counter!</h2>
   <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+
+  <div>
+    <button class="btn btn-primary" @click="increase">+1</button>
+    <button class="btn btn-secondary" @click="decrease">-1</button>
+  </div>
+
 </template>
 
 <script>
@@ -13,6 +19,12 @@ export default {
   methods: {
     getSquareValue() {
       return Math.pow(this.counter, 2)
+    },
+    increase() {
+      this.counter += 1
+    },
+    decrease() {
+      this.counter -= 1
     }
   },
   computed: {
@@ -24,5 +36,19 @@ export default {
 </script>
 
 <style>
+button {
+  background-color: #64BB87;
+  border-radius: 5px;
+  border: 1px solid white;
+  color: white; 
+  cursor: pointer;
+  margin: 0 5px;
+  padding: 5px 15px;
+  transition: 0.3s ease-in-out;
+}
 
+button:hover {
+  background-color: #5aa67b;
+  transition: 0.3s ease-in-out;
+}
 </style>
