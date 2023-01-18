9fc642f3ffe8075454a428ef328b213117d82e92
hecdelatorre <hector982015@gmail.com>
2023-01-17 20:56:05 -0600

    17-01-2023 20:56:05

--- src/components/Indecision.vue
+++ src/components/Indecision.vue
@@ -1,13 +1,79 @@
 <template>
-  <h1>Indecision</h1> 
+  <img src="https://via.placeholder.com/250" alt="250">
+  <div class="bg-dark"></div>
+  <div class="indecision-container">
+    <input type="text" placeholder="Hazme una pregunta" v-model="question" v-on:keypress.enter="displayQuestion">
+    <p>Recuerda treminar con un signo de interrogación (?)</p>
+
+    <div>
+      <h2>{{ question }}</h2>
+      <h1>Si, No ...</h1>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return{
+      question: null
+    }
+  },
+  watch: {
+    question(value, oldValue) {
+      if (!value.includes('?')) return
+      // TODO: Dealizar petición http
+    }
+  },
+  methods: {
+    displayQuestion() {
+      this.question = 'Hola'
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+    img, .bg-dark {
+        height: 100vh;
+        left: 0px;
+        max-height: 100%;
+        max-width: 100%;
+        position: fixed;
+        top: 0px;
+        width: 100vw;
+    }
 
+    .bg-dark {
+        background-color: rgba(0, 0, 0, 0.4);
+    }
+
+    .indecision-container {
+        position: relative;
+        z-index: 99;
+    }
+    
+    input {
+        width: 250px;
+        padding: 10px 15px;
+        border-radius: 5px;
+        border: none;
+    }
+    input:focus {
+        outline: none;
+    }
+
+    p {
+        color: white;
+        font-size: 20px;
+        margin-top: 0px;
+    }
+
+    h1, h2 {
+        color: white;
+    }
+    
+    h2 {
+        margin-top: 150px;
+    }
 </style>
