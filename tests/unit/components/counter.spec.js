eb75c492258524e4ea1ce0ec601853b72ad81d39
hecdelatorre <hector982015@gmail.com>
2023-01-19 12:40:49 -0600

    19-01-2023 12:40:49 Optimización

--- tests/unit/components/counter.spec.js
+++ tests/unit/components/counter.spec.js
@@ -2,13 +2,14 @@ import { shallowMount } from '@vue/test-utils'
 import Counter from '@/components/Counter'
 
 describe('Counter', () => {
+  let wrapper
+  beforeEach(() => wrapper = shallowMount(Counter))
 
   // test('Debe de hacer match con el snapshot', () => {
   //   const wrapper = shallowMount( Counter )
   //   expect( wrapper.html() ).toMatchSnapshot()
   // })
   test('h2 debe tener el valor por defecto "Counter"', () => {
-    const wrapper = shallowMount( Counter )
     // wrapper.findAll('h2')
     // expect(wrapper.find('h2').exists()).toBe(true)
     expect(wrapper.find('h2').exists()).toBeTruthy()
@@ -16,11 +17,19 @@ describe('Counter', () => {
     expect(h2Value).toBe('Counter')
   })
 
-  test('El valor por defecto debe ser 100 en el parrafo', () => {
-    const wrapper = shallowMount( Counter )
+  test('El valor por defecto debe ser 100 en el parrafo', async() => {
     const pValue = wrapper.find('[data-testid="counter"]').text()
     //expect(pValue[0].text()).toBe('100')
     expect(pValue).toBe('100')
+  })
+
+  test('Debe Aumuntar y disminuir el contador', async() => {
+    const [ increaseBtn, decreaseBtn ] = wrapper.findAll('button')
+
+    await increaseBtn.trigger('click')
+    const pValue = wrapper.find('[data-testid="counter"]').text()
+    expect(pValue).toBe('101')
+
   })
 
 })
