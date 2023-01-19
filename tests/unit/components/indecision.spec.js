8524e7af5a23c402b5fc1ec97002a2b2b2d34149
hecdelatorre <hector982015@gmail.com>
2023-01-19 16:27:47 -0600

    19-01-2023 16:27:47

--- tests/unit/components/indecision.spec.js
+++ tests/unit/components/indecision.spec.js
@@ -3,14 +3,21 @@ import Indecision from '@/components/Indecision'
 
 describe('Indecision', () => {
   let wrapper
-  beforeEach(() => wrapper = shallowMount( Indecision ))
+  let clgSpy
+  beforeEach(() => {
+    wrapper = shallowMount( Indecision )
+    clgSpy = jest.spyOn(console, 'log')
+  })
   test('Indecision - Snapshot', () => expect(wrapper.html()).toMatchSnapshot())
 
-  test('Escribir en el input no debe disparar nada (console.log)', () => {
-    
+  test('Escribir en el input no debe disparar nada (console.log)', async() => {
+    const input = wrapper.find('input')
+    await input.setValue('Hola mundo')
+    // expect(clgSpy).toHaveBeenCalled()
+    expect(clgSpy).toHaveBeenCalledTimes(1)
   })
 
-  test('Escribir "?" debe de disparar el fetch', () => {})
-  test('Pruebas en getAnswer', () => {})
-  test('Pruebas en getAnswer - fallo en el API', () => {})
+  // test('Escribir "?" debe de disparar el fetch', () => {})
+  // test('Pruebas en getAnswer', () => {})
+  // test('Pruebas en getAnswer - fallo en el API', () => {})
 })
