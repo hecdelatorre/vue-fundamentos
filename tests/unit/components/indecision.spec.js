17f52ef54e1738ba2459dff8c2876f9e0e19a70c
hecdelatorre <hector982015@gmail.com>
2023-01-19 20:25:43 -0600

    19-01-2023 20:25:43

--- tests/unit/components/indecision.spec.js
+++ tests/unit/components/indecision.spec.js
@@ -11,13 +11,17 @@ describe('Indecision', () => {
   test('Indecision - Snapshot', () => expect(wrapper.html()).toMatchSnapshot())
 
   test('Escribir en el input no debe disparar nada (console.log)', async() => {
+    const getAnswerSpy = jest.spyOn(wrapper.vm, 'getAnswer')
+
     const input = wrapper.find('input')
     await input.setValue('Hola mundo')
     // expect(clgSpy).toHaveBeenCalled()
     expect(clgSpy).toHaveBeenCalledTimes(1)
+    // expect(getAnswerSpy).toHaveReturnedTimes(0)
+    expect(getAnswerSpy).not.toHaveBeenCalled()
   })
 
-  // test('Escribir "?" debe de disparar el fetch', () => {})
+  // test('Escribir "?" debe de disparar el getAnswer', () => {})
   // test('Pruebas en getAnswer', () => {})
   // test('Pruebas en getAnswer - fallo en el API', () => {})
 })
